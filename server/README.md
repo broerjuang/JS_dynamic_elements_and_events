00d491d4c465ff9a39355fb1ff4256ee8b6508b3
broerjuang <juangwiantoro@gmail.com>
2016-11-12 20:18:47 +0700

    modifying app.js and creating some variables from it

---
+++ server/README.md
@@ -0,0 +1,9 @@
+# API Routing
+
+| Method    | Routes                 | Description           |
+| --------- |:----------------------:| ---------------------:|
+| GET       | /api/todos             | Get all todos         |
+| POST      | /api/todos             | Create a todo         |
+| GET       | /api/todos/:todo_id    | Get todo by its id    |
+| PUT       | /api/todos/:todo_id    | Update todo by its id |
+| DELETE    | /api/todos/:todo_id    | Delete todo by its id |
--- server/app.js
+++ server/app.js
@@ -4,9 +4,17 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var cors = require(cors);
+
+// require mongoose
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/todoList');
+
+// mongoose global promise
+mongoose.Promise = global.Promise;
 
 var routes = require('./routes/index');
-var users = require('./routes/users');
+var todo = require('./routes/todo.route');
 
 var app = express();
 
@@ -21,9 +29,10 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(cors())
 
 app.use('/', routes);
-app.use('/users', users);
+app.use('/api', todo);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
