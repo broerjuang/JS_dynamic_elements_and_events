92783d0e76fa21a125b981cb914aa416732c3da4
broerjuang <juangwiantoro@gmail.com>
2016-11-17 23:36:41 +0700

    testing all done

--- server/README.md
+++ server/README.md
@@ -7,6 +7,24 @@ Before starting the server please make sure you have following modules installed
 After all those stuff are available in your computer, you can `npm install` on your terminal. Then, you can start using `> npm start` in this directory.
 
 
+## Dependencies
+```js
+body-parser
+chai
+chai-http
+cookie-parser
+debug
+express
+faker
+jade
+mocha
+mongoose
+mongoose-sequence
+morgan
+serve-favicon
+```
+
+
 ## API Routing
 
 | Method    | Routes                 | Description           |
--- server/test/memo.test.js
+++ server/test/memo.test.js
@@ -28,57 +28,55 @@ describe('creating a new todo', () => {
   })
 })
 
-describe('update a to de')
+describe('Get a todo', () => {
+  it('expect to get a todo', () => {
+    chai.request(URL)
+      .get('/todos/16')
+      .end((req, res) => {
+        expect(res).to.have.status(200)
+        expect(res.body.content).to.be.a('string')
+        done()
+      })
+  })
+})
 
-// describe('Route get all kanbans', function() {
-//     it('expect to return object', function(done) {
-//             chai.request(urlApi)
-//                 .get('/cards')
-//                 .end(function(err, res) {
-//                     expect(typeof(res.body)).to.be.equal('object')
-//                     done()
-//                 }) // chai
-//         }) // it
-// })
-//
-// describe('Route update new kanban', function() {
-//     let selected = 4
-//     let arr = ['to-do', 'doing', 'done']
-//     let newTitle = faker.lorem.sentence()
-//     let newContent = faker.lorem.sentences()
-//     let newDate = faker.date.future()
-//     let newStatus = arr[Math.ceil(Math.random() * 2)]
-//     console.log(newStatus);
-//     let newPerson = faker.name.firstName()
-//     it('expect to return updated new kanban details', function(done) {
-//         chai.request(urlApi)
-//             .put(`/cards/${selected}`)
-//             .send({
-//                 title: newTitle,
-//                 content: newContent,
-//                 due_date: newDate,
-//                 status: newStatus,
-//                 in_charge: newPerson
-//             })
-//             .end(function(req, res) {
-//                 expect(res.body.title).to.be.equal(newTitle)
-//                 expect(res.body.content).to.be.equal(newContent)
-//                 expect(res.body.due_date).to.be.equal(newDate.toISOString())
-//                 expect(res.body.status).to.be.equal(newStatus)
-//                 expect(res.body.in_charge).to.be.equal(newPerson)
-//                 done()
-//             })
-//     })
-// })
-//
-// describe('Route delete a kanban', function() {
-//     let selected = 5
-//     it('expect to return deleted kanban', function(done) {
-//             chai.request(urlApi)
-//                 .delete(`/cards/${selected}`)
-//                 .end(function(err, res) {
-//                     expect(res.body.message).to.be.equal('delete successfull')
-//                     done()
-//                 }) // chai
-//         }) // it
-// })
+describe('update todo', () => {
+  let updateContent = faker.lorem.sentence()
+  it('expect todo to be updated', () => {
+    chai.request(URL)
+      .put('/todos/16')
+      .send({
+        content: updateContent
+      })
+      .end((req, rs) => {
+        expect(res.body.content).to.be.equal(updateContent)
+        done()
+      })
+  })
+})
+
+describe('get all available todo', () => {
+  it('expect to get all todos', () => {
+    chai.request(URL)
+      .get('/todos')
+      .end((req, res) => {
+        expect(res.body).to.be.equal('object')
+        expect(res).to.have.status(200)
+        done()
+      })
+  })
+})
+
+
+describe('delete a single todo', () => {
+  let newContent = faker.lorem.sentence()
+  it('expect to get all todos', () => {
+    chai.request(URL)
+      .delete('/todos/16')
+      .end((req, res) => {
+        expect(res.body.message).to.be.equal('it has been removed')
+        expect(res).to.have.status(200)
+        done()
+      })
+  })
+})
